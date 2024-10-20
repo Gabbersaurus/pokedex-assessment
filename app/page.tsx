caf97fb5682edbefb9fcee56b84b1ad900161b76
Pascal Bouwhuis <git@pascalbouwhuis.nl>
2024-10-20 17:47:45 +0200

    feat: add homepage content

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,47 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Homepage</div>;
+  return (
+    <div className="grow flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-center">
+        Pokédex Assessment
+        <span className="block text-2xl">for Blue Flamingos</span>
+      </h1>
+
+      <div className="p-4 bg-white rounded-lg shadow-md mt-10 max-w-xl">
+        <h2 className="text-2xl font-bold mb-4">About This Project</h2>
+        <p>
+          This assesment was made by{" "}
+          <a
+            href={"https://www.linkedin.com/in/pascal-bouwhuis/"}
+            target="_blank"
+            className="text-blue-600 hover:underline"
+          >
+            Pascal Bouwhuis
+          </a>{" "}
+          to demonstrate my frontend webdevelopment and API integration skills,
+          following Blue Flamingos' assesment description. I used a self-hosted
+          version of the GraphQL PokeAPI to fetch and display the Pokémon.
+        </p>
+        <p>
+          Head over to the{" "}
+          <Link href={"/list"} className="text-blue-600 hover:underline">
+            /list
+          </Link>{" "}
+          page to explore the Pokémon collection!
+        </p>
+        <p>
+          The source code for this assesment can be found on{" "}
+          <a
+            href={"https://github.com/Gabbersaurus/pokedex-assessment"}
+            target="_blank"
+            className="text-blue-600 hover:underline"
+          >
+            Github
+          </a>
+          .
+        </p>
+      </div>
+    </div>
+  );
 }
