707af24814f8c02cce2109da966b2f310839c1dd
Pascal Bouwhuis <git@pascalbouwhuis.nl>
2024-10-21 01:18:13 +0200

    fix: re-enable cache in GraphQL client

--- lib/pokeapi/client.ts
+++ lib/pokeapi/client.ts
@@ -1,21 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import type { DefaultOptions } from "@apollo/client";
-
-const defaultOptions: DefaultOptions = {
-  watchQuery: {
-    fetchPolicy: "no-cache",
-    errorPolicy: "ignore",
-  },
-  query: {
-    fetchPolicy: "no-cache",
-    errorPolicy: "all",
-  },
-};
 
 const client = new ApolloClient({
   uri: process.env.API_URL,
   cache: new InMemoryCache(),
-  defaultOptions: defaultOptions,
 });
 
 export default client;
