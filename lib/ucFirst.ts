1944bd2097f310680e524d3655e6ebbed59e7721
Pascal Bouwhuis <git@pascalbouwhuis.nl>
2024-10-21 08:48:58 +0200

    fix: resolve potential issues with ucFirst

--- lib/ucFirst.ts
+++ lib/ucFirst.ts
@@ -1,3 +1,5 @@
 export default function ucFirst(input: string) {
+  if (input.length < 0) return "";
+  if (input.length < 1) return input.toUpperCase();
   return input.charAt(0).toUpperCase() + input.slice(1);
 }
